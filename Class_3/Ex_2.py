20c886fcf7f676b43b8994823df16eae2e88089d
Bartosz Kunat <mrclapslock@gmail.com>
2017-10-28 12:24:05 +0200

    Code divided into simpler functions

--- Class_3/Ex_2.py
+++ Class_3/Ex_2.py
@@ -1,46 +1,40 @@
 
 def squareAnimation():
 
-    while True:
-
-        frameHeight = int(input("Please enter frame's height: \nRemember: Odd numbers only!\n"))
-        frameWidth = int(input("Please enter frame's width:\nRemember: Odd numbers only!\n"))
-
-        if (frameHeight % 2 != 0 and frameWidth % 2 != 0):
-            break
-
-    frameTemplate = [["o"] * frameWidth]*frameHeight
-
-    for outerListElement in range(0, len(frameTemplate)-1):
-        for innerListElement in range(0, len(frameTemplate[outerListElement])-1):
-            if outerListElement == int(frameHeight / 2) and innerListElement == int(frameWidth / 2):
-                frameTemplate[outerListElement] = "o " * int(frameWidth / 2) + "x "
-
-    for object in range(0, len(frameTemplate)):
-        tempLine = frameTemplate[object]
-        print(" ".join(tempLine))
-
-
-def squareAnimation2():
 
     while True:
 
         frameHeight = int(input("Please enter frame's height: \nRemember: Odd numbers only!\n"))
         frameWidth = int(input("Please enter frame's width:\nRemember: Odd numbers only!\n"))
 
+
         if (frameHeight % 2 != 0 and frameWidth % 2 != 0):
             break
+    numberOfFrames = min(int(frameWidth/2), int(frameHeight/2))
 
     lineTemplate = "o " * frameWidth
-    frameTemplate = [lineTemplate] * frameHeight
+    #frame filled with "o" only
+    blankFrame = [lineTemplate] * frameHeight
 
+    #frame with x in center
+    frameTemplate = blankFrame
     findListCenter(frameTemplate, frameHeight, frameWidth)
+    printListWithoutBrackets(frameTemplate)
 
-    for object in range(0, len(frameTemplate)):
-        tempLine = frameTemplate[object]
-        print("".join(tempLine))
+    for frameNumber in range(0, numberOfFrames):
+        currentFrame = blankFrame
+
+        
+
+
+
 
 def findListCenter(frame, height, width):
     frame[int(height / 2)] = "o " * (int(width / 2)) + "x " + "o " * (int(width / 2))
 
-squareAnimation2()
+def printListWithoutBrackets(frame):
+    for object in range(0, len(frame)):
+        tempLine = frame[object]
+        print("".join(tempLine))
+
+squareAnimation()
