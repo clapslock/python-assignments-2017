4c12c1fa5c3805461159f962a0adb023675290cf
Bartosz Kunat <mrclapslock@gmail.com>
2018-01-08 00:07:03 +0100

    finished last assignment (Class_10)

--- Class_10/Ex_1.py
+++ Class_10/Ex_1.py
@@ -1,44 +1,130 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
+
+class MyNode:
+
+    def __init__(self, score, name):
+        self.score = score
+        self.name = name
+        self.prev = None
         self.next = None
 
-    def __str__(self):
-        return str(self.data)
+class MyLinkedList:
 
-
-class UnidirectionalList:
     def __init__(self):
         self.head = None
-        self.size = 0
+        self.tail = None
+        self.count = 0
 
-    def add(self, data):
-        if not self.head:
-            n = Node(data)
+    def addAtBeginning(self, score, name):
+
+        n = MyNode(score, name)
+        self.count += 1
+
+        if self.head != None:
+            n.next = self.head
             self.head = n
         else:
+            self.head = n
+        if self.tail == None:
+            self.tail = n
+
+    def addAtEnd(self, score, name):
+
+        n = MyNode(score, name)
+        self.count += 1
+
+        if self.tail != None:
+            self.tail.next = n
+            n.prev = self.tail
+            self.tail = n
+
+        if self.head == None:
+            self.head = n
+
+    def addAtIndex(self, index, score, name):
+
+        n = MyNode(score, name)
+        tempNode = self.head
+        self.count += 1
+
+        if index == 0:
+            self.addAtBeginning(score, name)
+        elif index >= self.count:
+            self.addAtEnd(score, name)
+        elif index > 0 and index < self.count:
+            for i in range(1, index):
+                tempNode = tempNode.next
+
+            tempNode2 = tempNode.next
+            n.next = tempNode.next
+            tempNode.next = n
+            n.prev = tempNode
+            tempNode2.prev = n
+
+    def deleteLast(self):
+        if self.tail != None:
+            n = self.tail
+            self.tail = n.prev
+            n.prev = None
+            n.next = None
+            self.count -= 1
+
+    def deleteFirst(self):
+        if self.head != None:
             n = self.head
-            while n.next != None:
-                n = n.next
-            new_node = Node(data)
-            n.next = new_node;
+            self.head = n.next
+            n.next = None
 
-    def printList(self):
+    def printFromStart(self):
         n = self.head
-        while n:
-            print(n)
-            n = n.next
+
+        for i in range(self.count):
+            print(n.name)
+            if n.next != None:
+                n = n.next
+            else:
+                break
+
+    def printFromEnd(self):
+        n = self.tail
+
+        for i in range(self.count):
+            print(n.name)
+
+            if n.prev != None:
+                n = n.prev
+            else:
+                break
+
+    def findHighestScore(self):
+        hs = 0
+        n = self.head
+
+        for i in range(self.count):
+            if int(n.score) > hs:
+                hs = n.score
+
+            if n.next != None:
+                n = n.next
+            else:
+                break
+        return hs
+
+ll = MyLinkedList()
+ll.addAtBeginning(1, "Bartek")
+ll.addAtEnd(2, "Chloe")
+ll.addAtEnd(21, "Lana")
+ll.addAtBeginning(101, "Ellie")
+ll.addAtIndex(2, 42, "Mikky B")
+ll.printFromStart()
+print()
+ll.printFromEnd()
+print(ll.findHighestScore())
 
 
 
 
-if __name__ == '__main__':
 
-    ll = UnidirectionalList()
-    ll.add(14)
-    ll.add("test")
-    ll.add(2.34)
-    ll.add(True)
-    ll.insertAtIndex1("what its is?!")
 
-    ll.printList()
+
+
+
